f3aae8d0c1628e4f3365703c205540851158727a
Greg Gibeling <gdgib@outlook.com>
2021-04-07 17:52:01 -0700

    G2-1163 Use image clustering for comparisons

--- jo-ssg/src/test/java/com/g2forge/joint/ssg/TestJoint.java
+++ jo-ssg/src/test/java/com/g2forge/joint/ssg/TestJoint.java
@@ -10,7 +10,14 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.Collection;
 import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -20,15 +27,24 @@ import com.g2forge.alexandria.java.close.ICloseable;
 import com.g2forge.alexandria.java.close.ICloseableSupplier;
 import com.g2forge.alexandria.java.concurrent.HConcurrent;
 import com.g2forge.alexandria.java.core.helpers.HBinary;
+import com.g2forge.alexandria.java.core.helpers.HCollection;
+import com.g2forge.alexandria.java.core.helpers.HCollector;
 import com.g2forge.alexandria.java.core.resource.Resource;
 import com.g2forge.alexandria.java.function.IFunction1;
 import com.g2forge.alexandria.java.io.HIO;
-import com.g2forge.alexandria.java.io.file.CompareWalker;
 import com.g2forge.alexandria.java.io.file.CopyWalker;
 import com.g2forge.alexandria.java.io.file.TempDirectory;
+import com.g2forge.alexandria.java.io.file.compare.CompareWalker;
+import com.g2forge.alexandria.java.io.file.compare.IFileCompareGroup;
+import com.g2forge.alexandria.java.io.file.compare.IFileCompareGroupFunction;
+import com.g2forge.alexandria.java.io.file.compare.SHA1HashFileCompareGroupFunction;
+import com.g2forge.alexandria.java.io.file.compare.TextHashFileCompareGroupFunction;
 import com.g2forge.alexandria.media.IMediaType;
 import com.g2forge.alexandria.media.MediaType;
 import com.g2forge.alexandria.test.HAssert;
+import com.g2forge.gearbox.image.ImageCluster;
+import com.g2forge.gearbox.image.PHashImageComparator;
+import com.g2forge.gearbox.image.PHashImageComparator.CharacterizedImage;
 import com.g2forge.joint.core.ExtendedMediaType;
 import com.g2forge.joint.ssg.operation.Operation;
 
@@ -37,24 +53,47 @@ import lombok.Getter;
 import net.sourceforge.plantuml.security.ImageIO;
 
 public class TestJoint {
-	protected static final IFunction1<Path, IFunction1<Path, String>> HASHFUNCTIONFUNCTION = path -> {
+	protected static final int COMPARATOR_GROUPDISTANCE = 50;
+
+	protected static final PHashImageComparator COMPARATOR = new PHashImageComparator(16);
+
+	protected static final IFunction1<Path, IFileCompareGroupFunction<?>> HASHFUNCTIONFUNCTION = path -> {
 		final IMediaType mediaType = ExtendedMediaType.getRegistry().computeMediaType(path);
 
 		// If the media type is a PNG (probably from PlantUML) then just hash the image data, not the metadata
 		if (MediaType.PNG.equals(mediaType)) {
-			return actual -> {
-				try {
-					final byte[] expectedBytes = ((DataBufferByte) ImageIO.read(Files.newInputStream(actual)).getData().getDataBuffer()).getData();
-					return HBinary.toHex(HIO.sha1(expectedBytes, ByteArrayInputStream::new));
-				} catch (IOException e) {
-					throw new UncheckedIOException(e);
+			new IFileCompareGroupFunction<CharacterizedImage>() {
+				@Override
+				public Map<IFileCompareGroup, Set<Path>> group(Map<Path, ? extends CharacterizedImage> hashes) {
+					final Map<CharacterizedImage, Path> reverse = hashes.entrySet().stream().collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey, HCollector.mergeFail(), IdentityHashMap::new));
+					final List<ImageCluster<CharacterizedImage>> clusters = ImageCluster.cluster(COMPARATOR, hashes.values(), COMPARATOR_GROUPDISTANCE);
+					final Map<IFileCompareGroup, Set<Path>> retVal = new HashMap<>();
+					for (ImageCluster<CharacterizedImage> cluster : clusters) {
+						retVal.put(new IFileCompareGroup() {
+							@Override
+							public String describe(Collection<Path> roots, Path relative) {
+								try {
+									final byte[] expectedBytes = ((DataBufferByte) ImageIO.read(Files.newInputStream(HCollection.getAny(roots).resolve(relative))).getData().getDataBuffer()).getData();
+									return HBinary.toHex(HIO.sha1(expectedBytes, ByteArrayInputStream::new));
+								} catch (IOException e) {
+									throw new UncheckedIOException(e);
+								}
+							}
+						}, cluster.getImages().stream().map(reverse::get).collect(Collectors.toSet()));
+					}
+					return retVal;
+				}
+
+				@Override
+				public CharacterizedImage hash(Path path) {
+					return COMPARATOR.characterize(path);
 				}
 			};
 		}
 
 		// Fall back to the default hash functions
-		if ((mediaType != null) && mediaType.isText()) return CompareWalker.TextHashFunction.create();
-		return CompareWalker.BinaryHashFunction.create();
+		if ((mediaType != null) && mediaType.isText()) return TextHashFileCompareGroupFunction.create();
+		return SHA1HashFileCompareGroupFunction.create();
 	};
 
 	@Rule
@@ -63,7 +102,7 @@ public class TestJoint {
 
 	protected void compareBuild(final Path input, final Path expected, final Path actual) throws Exception {
 		HAssert.assertEquals(Integer.valueOf(0), createJoint(input, actual, Operation.Build).call());
-		CompareWalker.builder().root(actual).hashFunctionFunction(HASHFUNCTIONFUNCTION).build().walkFileTree(expected);
+		CompareWalker.builder().root(actual).groupFunctionFunction(HASHFUNCTIONFUNCTION).build().walkFileTree(expected);
 	}
 
 	@Test
@@ -104,17 +143,17 @@ public class TestJoint {
 			CopyWalker.builder().target(input).build().walkFileTree(source);
 
 			try (final ICloseable closeable = createJoint(input, output, Operation.Serve).start()) {
-				CompareWalker.builder().root(output).hashFunctionFunction(HASHFUNCTIONFUNCTION).build().walkFileTree(source);
+				CompareWalker.builder().root(output).groupFunctionFunction(HASHFUNCTIONFUNCTION).build().walkFileTree(source);
 
 				// Test that new files get copied correctly
 				Files.newBufferedWriter(input.resolve("dir1/text.txt"), StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE).append("Text").close();
 				HConcurrent.wait(500);
-				CompareWalker.builder().root(output).hashFunctionFunction(HASHFUNCTIONFUNCTION).build().walkFileTree(input);
+				CompareWalker.builder().root(output).groupFunctionFunction(HASHFUNCTIONFUNCTION).build().walkFileTree(input);
 
 				// Test that modified files get re-copied correctly
 				Files.newBufferedWriter(input.resolve("dir1/text.txt"), StandardOpenOption.APPEND, StandardOpenOption.WRITE).append("Other").close();
 				HConcurrent.wait(500);
-				CompareWalker.builder().root(output).hashFunctionFunction(HASHFUNCTIONFUNCTION).build().walkFileTree(input);
+				CompareWalker.builder().root(output).groupFunctionFunction(HASHFUNCTIONFUNCTION).build().walkFileTree(input);
 			}
 		}
 	}
