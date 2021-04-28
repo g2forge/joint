5381d4f894df3f0729ce11eaed320fbb4bb0b3e5
Greg Gibeling <gdgib@outlook.com>
2021-04-27 19:50:19 -0700

    G2-1195 Support link rewriting in config

--- jo-ui/src/app/notfound/notfound.component.ts
+++ jo-ui/src/app/notfound/notfound.component.ts
@@ -3,27 +3,54 @@ import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
 import { ToolbarComponent } from '../toolbar/toolbar.component';
+import { ConfigService, JointConfig } from '../config.service';
 
-@Component( {
-    selector: 'app-notfound',
-    templateUrl: './notfound.component.html',
-    styleUrls: ['./notfound.component.scss']
-} )
+@Component({
+	selector: 'app-notfound',
+	templateUrl: './notfound.component.html',
+	styleUrls: ['./notfound.component.scss']
+})
 export class NotfoundComponent implements OnInit {
-    url: string | null = null;
+	url: string | null = null;
+	rewrites?: any;
 
-    constructor(
-        private router: Router
-    ) { }
+	constructor(
+		private router: Router,
+		private configService: ConfigService
+	) { }
 
-    ngOnInit(): void {
-        this.load( this.router.url );
-        this.router.events.subscribe( event => {
-            if ( event instanceof NavigationEnd ) this.load( event.url );
-        } );
-    }
+	ngOnInit(): void {
+		this.load(this.router.url);
+		this.router.events.subscribe(event => {
+			if (event instanceof NavigationEnd) this.load(event.url);
+		});
 
-    load( url: string ): void {
-        this.url = url;
-    }
+		this.configService.getConfig().subscribe((data: JointConfig) => {
+			this.rewrites = data.rewrites;
+			this.rewrite();
+		});
+	}
+
+	load(url: string): void {
+		this.url = url;
+		this.rewrite();
+	}
+
+	rewrite() {
+		if (this.url == null) return;
+		if (this.rewrites == undefined) return;
+
+		var current = this.url;
+		console.log("Attempting to rewrite " + current);
+		for (let [key, value] of Object.entries(this.rewrites)) {
+			var replacement = String(value);
+			console.log(key + " -> " + replacement);
+			current = current.replace(new RegExp(key, ""), String(value));
+		}
+
+		if (this.url != current) {
+			console.log("Rewrote " + this.url + " to " + current);
+			this.router.navigate([current]);
+		} else console.log("Rewrite kept " + this.url + " as " + current);
+	}
 }
--- jo-ui/src/app/toolbar/toolbar.component.spec.ts
+++ jo-ui/src/app/toolbar/toolbar.component.spec.ts
@@ -1,34 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Title } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { ToolbarComponent } from './toolbar.component';
 
+import { ConfigService, JointConfig } from '../config.service';
+import { Observable, of } from 'rxjs';
+
 class MockTitle {
-    setTitle( title: string ): void { }
+	setTitle(title: string): void { }
 }
 
-describe( 'ToolbarComponent', () => {
-    let component: ToolbarComponent;
-    let fixture: ComponentFixture<ToolbarComponent>;
+class MockConfigService {
+	getConfig(): Observable<JointConfig> {
+		return of({ "title": "Test Title" })
+	}
+}
 
-    beforeEach( async () => {
-        let title = new MockTitle();
-        await TestBed.configureTestingModule( {
-            declarations: [ToolbarComponent],
-            imports: [RouterTestingModule, HttpClientTestingModule],
-            providers: [{ provide: Title, useValue: title }]
-        } ).compileComponents();
-    } );
+describe('ToolbarComponent', () => {
+	let component: ToolbarComponent;
+	let fixture: ComponentFixture<ToolbarComponent>;
 
-    beforeEach( () => {
-        fixture = TestBed.createComponent( ToolbarComponent );
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    } );
+	beforeEach(async () => {
+		let title = new MockTitle();
+		let configService = new MockConfigService();
+		await TestBed.configureTestingModule({
+			declarations: [ToolbarComponent],
+			imports: [RouterTestingModule],
+			providers: [{ provide: Title, useValue: title }, { provide: ConfigService, useValue: configService }]
+		}).compileComponents();
+	});
 
-    it( 'should create', () => {
-        expect( component ).toBeTruthy();
-    } );
-} );
+	beforeEach(() => {
+		fixture = TestBed.createComponent(ToolbarComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+});
